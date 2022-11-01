9cfb8ad842acf7940b4f7c157a01f9ebb4fb322d
a1971649453 <71438393+a1971649453@users.noreply.github.com>
2022-11-01 16:15:36 +0800

    HI commit no1

---
+++ crm/src/main/java/com/hspedu/Hi.java
@@ -0,0 +1,11 @@
+package com.hspedu;
+
+/**
+ * @author 金宗文
+ * @version 1.0
+ */
+public class Hi {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
